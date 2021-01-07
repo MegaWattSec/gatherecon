cc82a2c83b3062c1fea9dda6b0e48c65640dc2ac
ponderng <dwatts.comptech@gmail.com>
2021-01-06 20:54:37 -0500

    getsubdomains skeleton

--- components/getsubdomains.py
+++ components/getsubdomains.py
@@ -15,7 +15,39 @@ class GetSubdomains(Component):
         "shuffledns",
         "nscope",
     ]
+
     def __init__(self, target, scope):
         self.target = target
         self.scope = scope
         self.options = f"-d {self.target} -s {self.scope}"
+        self.subdomains_all = []
+
+    def install(self):
+        return 0
+
+    def create_dirs(self):
+        return 0
+
+    def check_input(self):
+        return 0
+
+    def bass(self):
+        return 0
+
+    def subfinder(self):
+        return 0
+
+    def github_subdomains(self):
+        return 0
+
+    def amass(self):
+        return 0
+
+    def resolve_all(self):
+        return 0
+
+    def check_scope(self):
+        return 0
+
+    def get_subs(self):
+        return self.subdomains_all
