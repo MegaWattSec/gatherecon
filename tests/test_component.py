49f6154db4e76c547deeb3999820e498c814fe01
= <=>
2021-02-04 21:23:35 -0500

    fix tests

--- tests/test_component.py
+++ tests/test_component.py
@@ -4,8 +4,9 @@ from pytest_mock import mocker
 from pathlib import Path
 from components.getsubdomains import GetSubdomains
 
-def test_install():
+def test_install_getsubdomains():
     resultdir = Path.home() / "assets" / "example.com" / "test"
+    resultdir.mkdir(parents=True, exist_ok=True)
     scope = {
         "handle": "example",
             "targets": {
@@ -25,15 +26,15 @@ def test_install():
     scopef = resultdir / f"scope.json"
     with open(scopef, "w+") as f:
         f.write(f"[{json.dumps(scope)}]")
-    mod = GetSubdomains(scopef, "example", resultdir)
+    mod = GetSubdomains("example", scopef, resultdir)
     # Save primary domains list
-    with open(mod.input[0], "w+") as f:
+    with open(resultdir / mod.input[0], "w+") as f:
         f.write("example.com")
-    # False here means a good result, any other result is error
-    check.is_false(mod.install())
+    assert mod.install() == 0
 
 def test_input():
     resultdir = Path.home() / "assets" / "example.com" / "test"
+    resultdir.mkdir(parents=True, exist_ok=True)
     scope = {
         "handle": "example",
             "targets": {
@@ -53,43 +54,17 @@ def test_input():
     scopef = resultdir / f"scope.json"
     with open(scopef, "w+") as f:
         f.write(f"[{json.dumps(scope)}]")
-    mod = GetSubdomains(scopef, "example", resultdir)
+    mod = GetSubdomains("example", scopef, resultdir)
     # Save primary domains list
-    with open(mod.input[0], "w+") as f:
+    with open(resultdir / mod.input[0], "w+") as f:
         f.write("example.com")
     for each in mod.input:
         check.is_true( Path(each).exists() )
 
-def test_install_getsubdomains():
-    resultdir = Path.home() / "assets" / "example.com" / "test"
-    scope = {
-        "handle": "example",
-            "targets": {
-                "out_of_scope": [
-                    {
-                        "asset_identifier": "*.example.com"
-                    },
-                ],
-                "in_scope": [
-                    {
-                        "asset_identifier": "api.example.com"
-                    },
-                ]
-            }
-    }
-    # Save scope as a file
-    scopef = resultdir / f"scope.json"
-    with open(scopef, "w+") as f:
-        f.write(f"[{json.dumps(scope)}]")
-    mod = GetSubdomains(scopef, "example", resultdir)
-    # Save primary domains list
-    with open(mod.input[0], "w+") as f:
-        f.write("example.com")
-    check.is_false(mod.install())    # false here is a good result
-
 def test_getsubdomains_amass():
     # This test takes a looong time to run
     resultdir = Path.home() / "assets" / "example.com" / "test"
+    resultdir.mkdir(parents=True, exist_ok=True)
     scope = {
         "handle": "example",
             "targets": {
@@ -109,15 +84,16 @@ def test_getsubdomains_amass():
     scopef = resultdir / f"scope.json"
     with open(scopef, "w+") as f:
         f.write(f"[{json.dumps(scope)}]")
-    mod = GetSubdomains(scopef, "example", resultdir)
+    mod = GetSubdomains("example", scopef, resultdir)
     # Save primary domains list
-    with open(mod.input[0], "w+") as f:
+    with open(resultdir / mod.input[0], "w+") as f:
         f.write("example.com")
     result = mod.amass("example.com")
-    check.is_false(result)
+    assert result == 0
 
 def test_getsubdomains_bass():
     resultdir = Path.home() / "assets" / "example.com" / "test"
+    resultdir.mkdir(parents=True, exist_ok=True)
     scope = {
         "handle": "example",
             "targets": {
@@ -137,15 +113,16 @@ def test_getsubdomains_bass():
     scopef = resultdir / f"scope.json"
     with open(scopef, "w+") as f:
         f.write(f"[{json.dumps(scope)}]")
-    mod = GetSubdomains(scopef, "example", resultdir)
+    mod = GetSubdomains("example", scopef, resultdir)
     # Save primary domains list
-    with open(mod.input[0], "w+") as f:
+    with open(resultdir / mod.input[0], "w+") as f:
         f.write("example.com")
     result = mod.bass("example.com")
-    check.is_false(result)
+    assert result == 0
 
 def test_getsubdomains_subfinder():
     resultdir = Path.home() / "assets" / "example.com" / "test"
+    resultdir.mkdir(parents=True, exist_ok=True)
     scope = {
         "handle": "example",
             "targets": {
@@ -165,15 +142,16 @@ def test_getsubdomains_subfinder():
     scopef = resultdir / f"scope.json"
     with open(scopef, "w+") as f:
         f.write(f"[{json.dumps(scope)}]")
-    mod = GetSubdomains(scopef, "example", resultdir)
+    mod = GetSubdomains("example", scopef, resultdir)
     # Save primary domains list
-    with open(mod.input[0], "w+") as f:
+    with open(resultdir / mod.input[0], "w+") as f:
         f.write("example.com")
     result = mod.subfinder("example.com")
-    check.is_false(result)
+    assert result == 0
 
 def test_getsubdomains_hakrawler():
     resultdir = Path.home() / "assets" / "example.com" / "test"
+    resultdir.mkdir(parents=True, exist_ok=True)
     scope = {
         "handle": "example",
             "targets": {
@@ -193,15 +171,16 @@ def test_getsubdomains_hakrawler():
     scopef = resultdir / f"scope.json"
     with open(scopef, "w+") as f:
         f.write(f"[{json.dumps(scope)}]")
-    mod = GetSubdomains(scopef, "example", resultdir)
+    mod = GetSubdomains("example", scopef, resultdir)
     # Save primary domains list
-    with open(mod.input[0], "w+") as f:
+    with open(resultdir / mod.input[0], "w+") as f:
         f.write("example.com")
     result = mod.hakrawler("example.com")
-    check.is_false(result)
+    assert result == 0
 
 def test_getsubdomains_resolve():
     resultdir = Path.home() / "assets" / "example.com" / "test"
+    resultdir.mkdir(parents=True, exist_ok=True)
     scope = {
         "handle": "example",
             "targets": {
@@ -221,16 +200,17 @@ def test_getsubdomains_resolve():
     scopef = resultdir / f"scope.json"
     with open(scopef, "w+") as f:
         f.write(f"[{json.dumps(scope)}]")
-    mod = GetSubdomains(scopef, "example", resultdir)
+    mod = GetSubdomains("example", scopef, resultdir)
     # Save primary domains list
-    with open(mod.input[0], "w+") as f:
+    with open(resultdir / mod.input[0], "w+") as f:
         f.write("example.com")
     mod.all_subdomains = ["test.example.com", "test2.example.com", "test3.example.com"]
     result = mod.resolve_all()
-    check.is_false(result)
+    assert result == 0
 
 def test_getsubdomains_subscraper():
     resultdir = Path.home() / "assets" / "example.com" / "test"
+    resultdir.mkdir(parents=True, exist_ok=True)
     scope = {
         "handle": "example",
             "targets": {
@@ -250,15 +230,16 @@ def test_getsubdomains_subscraper():
     scopef = resultdir / f"scope.json"
     with open(scopef, "w+") as f:
         f.write(f"[{json.dumps(scope)}]")
-    mod = GetSubdomains(scopef, "example", resultdir)
+    mod = GetSubdomains("example", scopef, resultdir)
     # Save primary domains list
-    with open(mod.input[0], "w+") as f:
+    with open(resultdir / mod.input[0], "w+") as f:
         f.write("example.com")
     result = mod.subscraper("example.com")
-    check.is_false(result)
+    assert result == 0
 
 def test_check_scope_negative():
     resultdir = Path.home() / "assets" / "example.com" / "test"
+    resultdir.mkdir(parents=True, exist_ok=True)
     scope = {
         "handle": "example",
             "targets": {
@@ -278,15 +259,16 @@ def test_check_scope_negative():
     scopef = resultdir / f"scope.json"
     with open(scopef, "w+") as f:
         f.write(f"[{json.dumps(scope)}]")
-    mod = GetSubdomains(scopef, "example", resultdir)
+    mod = GetSubdomains("example", scopef, resultdir)
     # Save primary domains list
-    with open(mod.input[0], "w+") as f:
+    with open(resultdir / mod.input[0], "w+") as f:
         f.write("example.com")
     result = mod.check_scope("mapi.example.com", "example").decode('utf-8')
     check.is_not_in("mapi.example.com", result)
 
 def test_check_scope_postive():
     resultdir = Path.home() / "assets" / "example.com" / "test"
+    resultdir.mkdir(parents=True, exist_ok=True)
     scope = {
         "handle": "example",
             "targets": {
@@ -306,9 +288,9 @@ def test_check_scope_postive():
     scopef = resultdir / f"scope.json"
     with open(scopef, "w+") as f:
         f.write(f"[{json.dumps(scope)}]")
-    mod = GetSubdomains(scopef, "example", resultdir)
+    mod = GetSubdomains("example", scopef, resultdir)
     # Save primary domains list
-    with open(mod.input[0], "w+") as f:
+    with open(resultdir / mod.input[0], "w+") as f:
         f.write("example.com")
     result = mod.check_scope("api.example.com", "example").decode('utf-8')
     check.is_not("", result)
@@ -317,6 +299,7 @@ def test_check_scope_postive():
 def test_run_getsubdomains(mocker):
     # mock the methods that run tools to just test the "run" functionality
     resultdir = Path.home() / "assets" / "example.com" / "test"
+    resultdir.mkdir(parents=True, exist_ok=True)
     scope = {
         "handle": "example",
             "targets": {
@@ -336,9 +319,9 @@ def test_run_getsubdomains(mocker):
     scopef = resultdir / f"scope.json"
     with open(scopef, "w+") as f:
         f.write(f"[{json.dumps(scope)}]")
-    mod = GetSubdomains(scopef, "example", resultdir)
+    mod = GetSubdomains("example", scopef, resultdir)
     # Save primary domains list
-    with open(mod.input[0], "w+") as f:
+    with open(resultdir / mod.input[0], "w+") as f:
         f.write("example.com")
     mocker.patch(
         "components.getsubdomains.GetSubdomains.bass",
@@ -361,4 +344,4 @@ def test_run_getsubdomains(mocker):
         return_value=0
     )
     result = mod.run()
-    check.is_false(result)
+    assert result == 0
